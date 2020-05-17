ba30840eb92a5426ed430834e7089f9f522c55aa
Esmeralda Hdez <esmeyhg@gmail.com>
2020-05-16 19:01:14 -0500

    cambios minimos

--- control-egresados/resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ control-egresados/resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -33,13 +33,13 @@
         <ul class="sidebar-menu">
             <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
+            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>Egresados</span></a></li>
+            <li><a href="#"><i class='fa fa-link'></i> <span>Empresas</span></a></li>
             <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <a href="#"><i class='fa fa-link'></i> <span>Encuestas</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
+                    <li><a href="#">Seguimiento egresados</a></li>
+                    <li><a href="#">Sector productivo</a></li>
                 </ul>
             </li>
         </ul><!-- /.sidebar-menu -->
